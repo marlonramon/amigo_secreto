e6775fb7252f2037e60ac0c8c203146e445e272f
Marlon <marlon@marlon-HP-ProBook-4530s.(none)>
2014-11-25 22:14:24 -0200

    alteracao da base e migracao para rails 4

--- amigo_secreto/config/initializers/secret_token.rb
+++ amigo_secreto/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AmigoSecreto::Application.config.secret_token = '9371e358cc3fd5a05d003df6bb57b85dd3ad6e00cf04c0cac58692456cad37d730720e1b038fb6a782ae2d6d91c76ebf047f7fffb6ad22994ebf0fea160a538d'
+AmigoSecreto::Application.config.secret_token = '5f11839bf4c114abd28f3c02c71ad6a641cc7b3ad3b65ad746d4ba9db471a490a9eca1a1974896edda9dd1f0489776b261119c891219f135a4fb17c1b2d5423b'
--- amigo_secreto/config/routes.rb
+++ amigo_secreto/config/routes.rb
@@ -1,10 +1,4 @@
 AmigoSecreto::Application.routes.draw do
-  
-
-  resources :usuarios do
-     resources :comentarios
-  end
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,11 +48,16 @@ AmigoSecreto::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'usuarios#index'
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  resources :usuarios do
+    resources :comentarios do
+    end
+  end
+
 end
