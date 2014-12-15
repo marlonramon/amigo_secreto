fe36a8fd928b2c063dc5d7293e0f76966d35b161
Marlon Ramon <marlonramon@gmail.com>
2014-12-15 14:15:02 -0200

    correcao

--- amigo_secreto/config/application.rb
+++ amigo_secreto/config/application.rb
@@ -4,9 +4,9 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module AmigoSecreto
@@ -27,11 +27,12 @@ module AmigoSecreto
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :'pt-BR'
+
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
